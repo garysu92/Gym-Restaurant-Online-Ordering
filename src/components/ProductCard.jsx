d94b482cf5e2acb5a420190c883bccf33f05338b
garysu92 <garysu929292@gmail.com>
2023-07-30 01:54:34 -0400

    styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,11 @@
 export const ProductCard = ({ product }) => {
     return (
-        <>
+        <div className="w-full p-4 m-2 rounded-2xl text-center bg-black">
             <img src={product.imageUrl} alt={product.name}></img>
             <div>{product.name}</div>
             <div>{product.adjective}</div>
             <div>{product.description}</div>
             <div>{product.price}</div>
-        </>
+        </div>
     )
 }
